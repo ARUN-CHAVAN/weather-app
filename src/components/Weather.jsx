b5399585f502413c7fd08a1033eb7a46f7ecb6f6
Arun chavan <chavanarun784@gmail.com>
2026-03-24 19:46:13 +0530

    added default city on load

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -11,38 +11,38 @@ function Weather() {
   const [history, setHistory] = useState([]);
 
   useEffect(() => {
-  const saved = JSON.parse(localStorage.getItem("history"));
-  if (saved) setHistory(saved);
-  setCity();
-  fetchDefaultWeather("Bengaluru");
-}, []);
+    const saved = JSON.parse(localStorage.getItem("history"));
+    if (saved) setHistory(saved);
+    setCity();
+    fetchDefaultWeather("Bengaluru");
+  }, []);
   const API_KEY = import.meta.env.VITE_API_KEY;
 
   const fetchDefaultWeather = async (defaultCity) => {
-  try {
-    setLoading(true);
+    try {
+      setLoading(true);
 
-    const res = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${API_KEY}&units=metric`
-    );
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${API_KEY}&units=metric`,
+      );
 
-    const forecastRes = await axios.get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${defaultCity}&appid=${API_KEY}&units=metric`
-    );
+      const forecastRes = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${defaultCity}&appid=${API_KEY}&units=metric`,
+      );
 
-    const dailyData = forecastRes.data.list.filter(
-      (item, index) => index % 8 === 0
-    );
+      const dailyData = forecastRes.data.list.filter(
+        (item, index) => index % 8 === 0,
+      );
 
-    setData(res.data);
-    setForecast(dailyData);
-    setError("");
-  } catch {
-    setError("Default city not loaded");
-  } finally {
-    setLoading(false);
-  }
-};
+      setData(res.data);
+      setForecast(dailyData);
+      setError("");
+    } catch {
+      setError("Default city not loaded");
+    } finally {
+      setLoading(false);
+    }
+  };
   const getWeather = async () => {
     if (!city) {
       setError("Please enter a city");
@@ -55,13 +55,9 @@ function Weather() {
       const res = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
       );
-
-      // Forecast
       const forecastRes = await axios.get(
         `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`,
       );
-
-      // Filter: one item per day (every 8th item ≈ 24h)
       const dailyData = forecastRes.data.list.filter(
         (item, index) => index % 8 === 0,
       );
@@ -94,7 +90,6 @@ function Weather() {
           const res = await axios.get(
             `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`,
           );
-
           setData(res.data);
           setError("");
         } catch {
@@ -108,13 +103,10 @@ function Weather() {
   // Dynamic background
   const getBackground = () => {
     if (!data) return "bg-light";
-
     const weather = data.weather[0].main;
-
     if (weather === "Clear") return "bg-warning";
     if (weather === "Rain") return "bg-dark text-white";
     if (weather === "Clouds") return "bg-secondary text-white";
-
     return "bg-info text-white";
   };
 
@@ -122,69 +114,60 @@ function Weather() {
     <div
       className={`container-fluid d-flex justify-content-center  align-items-start min-vh-100 ${getBackground()}`}
     >
-      <div className="card shadow p-4 w-100" style={{ maxWidth: "900px",width:"100%" }}>
+      <div
+        className="card shadow p-4 w-100"
+        style={{ maxWidth: "900px", width: "100%" }}
+      >
         <h3 className="text-center mb-3">Weather App</h3>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control mb-2"
+            placeholder="Enter city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && getWeather()}
+          />
 
-        {/* Input */}
-       
-       <div className="mb-3">
-  <input
-    type="text"
-    className="form-control mb-2"
-    placeholder="Enter city"
-    value={city}
-    onChange={(e) => setCity(e.target.value)}
-    onKeyDown={(e) => e.key === "Enter" && getWeather()}
-  />
-
-  <div className="d-flex gap-2 flex-wrap">
-    <button className="btn btn-primary w-50" onClick={getWeather}>
-      Search
-    </button>
-
-    <button className="btn btn-success w-50" onClick={getLocationWeather}>
-      My Location
-    </button>
-  </div>
-</div>
-          {history.length > 0 && (
-            <div className="mb-3">
-              <small>Recent:</small>
-              <div>
-                {history.map((item, index) => (
-                  <button
-                    key={index}
-                    className="btn btn-sm btn-outline-secondary m-1"
-                    onClick={() => {
-                      setCity(item);
-                    }}
-                  >
-                    {item}
-                  </button>
-                ))}
-              </div>
-            </div>
-          )}
-
-        {/* Error */}
-        {error && <div className="alert alert-danger">{error}
-          </div>}
-
-        {/* Loader */}
-        {loading && (
-          <div className="text-center">
-            <div className="spinner-border text-primary">
-
+          <div className="d-flex gap-2 flex-wrap">
+            <button className="btn btn-primary w-50" onClick={getWeather}>
+              Search
+            </button>
+            <button
+              className="btn btn-success w-50"
+              onClick={getLocationWeather}
+            >
+              My Location
+            </button>
+          </div>
+        </div>
+        {history.length > 0 && (
+          <div className="mb-3">
+            <small>Recent:</small>
+            <div>
+              {history.map((item, index) => (
+                <button
+                  key={index}
+                  className="btn btn-sm btn-outline-secondary m-1"
+                  onClick={() => {
+                    setCity(item);
+                  }}
+                >
+                  {item}
+                </button>
+              ))}
             </div>
           </div>
         )}
-
-        {/* Weather Data */}
+        {error && <div className="alert alert-danger">{error}</div>}
+        {loading && (
+          <div className="text-center">
+            <div className="spinner-border text-primary"></div>
+          </div>
+        )}
         {data && !loading && (
           <div className="text-center">
             <h4>{data.name}</h4>
-
-            {/* Weather Icon */}
             <img
               src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
               alt="weather"
@@ -198,35 +181,34 @@ function Weather() {
           </div>
         )}
         {forecast.length > 0 && !loading && (
-  <div className="mt-4">
-    <h5 className="text-center">5-Day Forecast</h5>
+          <div className="mt-4">
+            <h5 className="text-center">5-Day Forecast</h5>
 
-    <div className="d-flex justify-content-center flex-wrap">
-      {forecast.map((item, index) => (
-        <div
-          key={index}
-          className="card p-2 m-1 text-center flex-fill"
-          style={{ width: "120px" }}
-        >
-          <small>
-            {new Date(item.dt_txt).toLocaleDateString("en-US", {
-              weekday: "short",
-            })}
-          </small>
+            <div className="d-flex justify-content-center flex-wrap">
+              {forecast.map((item, index) => (
+                <div
+                  key={index}
+                  className="card p-2 m-1 text-center flex-fill"
+                  style={{ width: "100px" }}
+                >
+                  <small>
+                    {new Date(item.dt_txt).toLocaleDateString("en-US", {
+                      weekday: "short",
+                    })}
+                  </small>
 
-          <img
-            src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-            alt="icon"
-          />
+                  <img
+                    src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                    alt="icon"
+                  />
 
-          <small>{Math.round(item.main.temp)}°C</small>
-        </div>
-      ))}
-    </div>
-  </div>
-)}
-        </div>
-      
+                  <small>{Math.round(item.main.temp)}°C</small>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
